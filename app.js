9c6f92f8e793758174d5a48817f3da3aacbb8e71
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-06-07 19:40:53 +0530

    update todo design

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const todoForm = document.querySelector("#todoForm");
 const calendar = document.querySelector("#calendar");
 const taskFlag = document.querySelector("#flag");
 const todoTaskBody = document.querySelector("#todoTaskBody");
+const taskDone = document.querySelector("#taskDone");
 
-
+const LOCAL_STORAGE_TODO_KEY = "tasks.list";
+const todoLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODO_KEY)) || [];
 // event 
 openInput.addEventListener("click", function () {
     todoForm.classList.add("openTodoForm");
@@ -27,6 +29,17 @@ taskFlag.addEventListener("click", function () {
         taskFlag.classList.add("p3Task");
     }
 })
+// date and time
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
 
 function createTask() {
     const options = {
@@ -38,24 +51,69 @@ function createTask() {
     };
 
     let eventtime = document.querySelector(".datepicker-inputs").value;
-    const dateTimes = new Date(eventtime);
-    dateTimes.setMinutes(dateTimes.getMinutes() - dateTimes.getTimezoneOffset());
-    console.log(dateTimes.toISOString().slice(0, 16));
-    const dateTime = dateTimes.toLocaleDateString(undefined, options)
     const taskPriority = document.querySelector("#taskPriority").value;
     const taskTitle = document.querySelector("#todoTitle").value;
+    const taskHour = document.querySelector("#taskHour").value;
+    const taskMin = document.querySelector("#taskMin").value;
 
-    let creatTasks = document.createElement("div");
-    creatTasks.classList.add("borderRadius");
-    creatTasks.classList.add("w-33");
-    creatTasks.classList.add(`${taskPriority}Task`);
-    creatTasks.innerHTML = `<span id="taskTag"> <i class="fas fa-solid fa-bookmark"></i></span>
-    <h3>${taskTitle}</h3>
+    var target = new Date(eventtime);
+    const current = formatAMPM(target);
+    console.log(current)
+
+    const list = createList(taskTitle, current, taskPriority);
+    todoLists.push(list);
+    saveAndRender();
+}
+
+// save to local storage
+function saveToTask() {
+    localStorage.setItem(LOCAL_STORAGE_TODO_KEY, JSON.stringify(todoLists));
+}
+
+function createList(name, dateTime, flag) {
+    return {
+        id: Date.now().toString(),
+        name: name,
+        dateTime: dateTime,
+        flag: flag
+    }
+}
+
+function renderList() {
+    clearElement(todoTaskBody);
+    todoLists.forEach(list => {
+        let creatTasks = document.createElement("div");
+        creatTasks.classList.add("borderRadius");
+        creatTasks.classList.add("w-33");
+        creatTasks.classList.add(`${list.flag}Task`);
+        let currentTime = new Date();
+        let crntTime = formatAMPM(currentTime);
+        if (list.dateTime > crntTime) {
+            console.log("yes");
+        } else {
+            creatTasks.classList.add("onGoingTask");
+        }
+        creatTasks.innerHTML = `<span id="taskTag"> <i class="fas fa-solid fa-bookmark"></i></span>
+    <h3>${list.name}</h3>
             <div class="flex flexCenter flexBetween">
                 <span id="taskDone"> <i class="fas fa-1x fa-solid fa-check"></i></span>
                 <span id="taskTime" class="flex flexCenter "><i class="fas fa-solid fa-clock"></i>
-                    <pre>${dateTime}</pre></span>
-            </div>`;
-    console.log(creatTasks);
-    todoTaskBody.appendChild(creatTasks);
+                    <pre>${list.dateTime}</pre></span>
+            </div>
+            <span id="TaskTime"> On Going Task </span>
+            `;
+        todoTaskBody.appendChild(creatTasks);
+    });
 }
+
+function saveAndRender() {
+    saveToTask();
+    renderList();
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+renderList(); // render all list 
